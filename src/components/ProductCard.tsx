5269c2b6f7be764804b267b188548362916e9af9
FranciscoZhou5 <fg45874587@proton.me>
2022-10-21 18:20:26 -0300

    Fix not found module

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -30,7 +30,6 @@ export default function ProductCard({ product }: ProductCardProps) {
   }
 
   return (
-    // <Link passHref href={`/product/${product.id}`}>
     <div className="bg-card relative group rounded-md py-3">
       <label
         className="z-20 absolute right-0 top-0 w-14 h-14 flex justify-center items-center cursor-pointer"
@@ -65,35 +64,5 @@ export default function ProductCard({ product }: ProductCardProps) {
         </a>
       </Link>
     </div>
-    // </Link>
   );
-}
-
-{
-  /* <Link href={`/product/${prod.id}`} key={prod.id} passHref>
-                  <a className="bg-white100 group cursor-pointer h-68 rounded-lg flex flex-col items-center justify-between py-3">
-                    <div className="relative w-36 h-36">
-                      <img src={prod.images.thumb} className="object-cover duration-200 group-hover:scale-110" />
-                    </div>
-
-                    <div className="mt-1 flex flex-col justify-start w-full px-3">
-                      <div className="flex justify-between items-center">
-                        <h2> {prod.title} </h2>
-                      </div>
-
-                      <div className="w-full flex"></div>
-
-                      <div className="flex items-center">
-                        <h2>{formatCurrency(prod.hasDiscount ? prod.price.withDiscount : prod.price.value)}</h2>
-                        <span className="line-through">{prod.hasDiscount && formatCurrency(prod.price.value)}</span>
-                      </div>
-
-                      // <p className="text-sm"> {prod.description.short.slice(0, 60) + "..."} </p>
-
-                      <button className="mt-2 duration-200 hover:bg-transparent border-sky border-2 bg-sky px-3 py-2 rounded-md font-bold">
-                        Adicionar no carrinho
-                      </button>
-                    </div>
-                  </a>
-                </Link> */
 }
